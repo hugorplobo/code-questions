75bbf7bd56e92007e4934ae0c252d7e8a9a9b74a
Antônio Hugo <hugorplobo@gmail.com>
2022-12-15 01:25:31 -0300

    feat: add supabase provider and setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-import SignIn from "./pages/SignIn";
+import Auth from "./routes/Auth";
+import Error from "./routes/Error";
+import Home from "./routes/Home";
 import { useColorMode } from "@chakra-ui/react";
+import { createClient } from "@supabase/supabase-js";
+import { createContext } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Auth />,
+    errorElement: <Error />
+  },
+  {
+    path: "/app",
+    element: <Home />
+  }
+]);
+
+const supabaseClient = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_ANON_KEY
+);
+
+export const supabaseContext = createContext(supabaseClient);
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -8,7 +32,11 @@ function App() {
     toggleColorMode();
   }
 
-  return <SignIn />;
+  return (
+    <supabaseContext.Provider value={supabaseClient}>
+      <RouterProvider router={router} />
+    </supabaseContext.Provider>
+  )
 }
 
 export default App;
