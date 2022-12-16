ad02b4471e1196a69674183e4ec3354c542fc603
Antônio Hugo <hugorplobo@gmail.com>
2022-12-15 21:07:07 -0300

    feat: add side menu in home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,10 @@
+import SideMenu from "../components/SideMenu";
+import { Container } from "@chakra-ui/react";
+
 export default function Home() {
-    return <h1>Hello World</h1>
+    return (
+        <Container maxW="full" p="0">
+            <SideMenu />
+        </Container>
+    );
 }
