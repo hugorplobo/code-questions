fbd4e7af545184577e4dffa907d04b16ad3b9884
Antônio Hugo <hugorplobo@gmail.com>
2022-12-16 23:46:38 -0300

    feat: add handler for new topics and add current topic feedback

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,26 +1,40 @@
-import { Box, VStack, Heading, Divider, Text, Spinner, Button } from "@chakra-ui/react";
+import { Box, VStack, Heading, Divider, Text, Spinner, Button, useDisclosure } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
-import { useNavigate } from "react-router-dom";
-import { supabaseContext } from "../App";
+import { useNavigate, useParams } from "react-router-dom";
+import { supabaseContext } from "../context/SupabaseContext";
 import { useContext } from "react";
 import useSWR from "swr";
+import NewTopicModal from "./NewTopicModal";
 
 export default function SideMenu() {
   const supabase = useContext(supabaseContext);
+  const { topicName } = useParams<"topicName">();
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const fetcher = async () => {
     return await supabase.from("assunto").select("nome").throwOnError().then(res => res);
   }
 
-  const { data, error, isLoading } = useSWR("assuntos", fetcher);
+  const { data, error, isLoading, mutate } = useSWR("assuntos", fetcher);
 
   if (error) {
     console.error(error);
   }
 
+  const handleAddNewTopic = (name: string) => {
+    const newData = { ...data! };
+    newData.data?.push({ nome: name });
+    mutate(newData);
+  }
+
+  const generateTopicRoute = (name: string) => {
+    return "/app/" + name.split(" ").join("-");
+  }
+
   return (
-    <Box w="56" h="100vh" bg="gray.700" overflowY="scroll">
+    <Box w="250px" h="100vh" bg="gray.700" overflowY="scroll">
+      <NewTopicModal isOpen={isOpen} onClose={onClose} onAdd={handleAddNewTopic} />
       <VStack spacing="2" py="5" px="2.5">
         <Heading size="md">Conteúdos</Heading>
         <Divider></Divider>
@@ -31,16 +45,22 @@ export default function SideMenu() {
                 { data.data.map((item: { nome: string }) => (
                   <Button
                     key={item.nome}
-                    variant="ghost"
+                    variant={topicName && topicName.split("-").join(" ") == item.nome ? "solid" : "ghost"}
                     colorScheme="teal"
                     w="full"
-                    onClick={e => navigate(`/app/${item.nome}`)}
+                    onClick={e => navigate(generateTopicRoute(item.nome))}
                   >
                     {item.nome}
                   </Button>
                 )) }
 
-                <Button variant="outline" colorScheme="teal" w="full" leftIcon={<AddIcon />}>
+                <Button
+                  variant="outline"
+                  colorScheme="teal"
+                  w="full"
+                  leftIcon={<AddIcon />}
+                  onClick={onOpen}
+                >
                   Novo
                 </Button>
               </>
